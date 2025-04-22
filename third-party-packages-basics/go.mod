2cec5edd9b38d2f5af44d014c84d70696ddb2943
PraveenBalaji003 <praveenmj003@gmail.com>
2025-04-22 12:30:32 +0530

    Third Party Packages Basics

---
+++ third-party-packages-basics/go.mod
@@ -0,0 +1,5 @@
+module third-party-packages-basics
+
+go 1.24.2
+
+require github.com/Praveenmj005/calculator v0.0.0-20250422064616-89292190b661
---
+++ third-party-packages-basics/main.go
@@ -0,0 +1,18 @@
+package main
+
+//get from main - go get github.com/Praveenmj005/calculator@main
+//get from latest git tag version - go get github.com/Praveenmj005/calculator@lates
+//clean mod cache - go clean -modcache
+import (
+	"fmt"
+
+	"github.com/Praveenmj005/calculator"
+)
+
+func main() {
+	fmt.Println("1 + 1 = ", calculator.Add(1, 1))
+	fmt.Println("1 - 1 = ", calculator.Sub(1, 1))
+	fmt.Println("2 * 2 = ", calculator.Mutiply(2, 2))
+	fmt.Println("1 + 1 = ", calculator.Divide(3, 3))
+	fmt.Println("1 + 1 = ", calculator.Modulo(1, 1))
+}
